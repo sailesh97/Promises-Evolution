75fd74f7fb7c55aeac0aa1ddbafe9961dbfd5c3c
sailesh97 <saileshpatra997@gmail.com>
2022-10-19 01:25:08 +0530

    Promises

---
+++ 5 Promises.js
@@ -0,0 +1,23 @@
+console.log('start');
+
+const sub = new Promise((resolve2, reject2) => {
+    setTimeout(() => {
+      const result = true;  
+      if(result) resolve2("Subscribe to Roadside Coder");
+      else reject2(new Error("Why aren't you subscribe to Roadside Coder?"));
+    }, 2000);
+});
+
+sub.then((res) => {
+    console.log(res)
+}).catch((err) => {
+    console.log(err);
+})
+
+console.log('stop');
+
+/* Problems solved by Promise:
+    1. Inversion of control
+    2. Reduce lengthy code horizontally
+    3. Separation of concerns
+*/
