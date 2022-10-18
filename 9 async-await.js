4163da1a5d710c06eeac4351b70857f5b97a3bd3
sailesh97 <saileshpatra997@gmail.com>
2022-10-19 02:01:39 +0530

    async-await behaviour 1

---
+++ 9 async-await.js
@@ -0,0 +1,46 @@
+// More realistic example
+
+console.log("Start")
+
+function importantAction(username){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Subscribe to ${username}`);
+        },1000);
+    })
+}
+
+function likeTheVideo(video){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Like the ${video} video`)
+        }, 100);
+    })
+}
+
+function shareTheVideo(video){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Share the ${video} video`)
+        }, 500);
+    })
+}
+
+const result = async () => {
+    const message1 = await importantAction("Roadside Coder");
+    const message2 = await likeTheVideo("Javascript Interview Questions");
+    const message3 = await shareTheVideo("Javascript Interview Questions");
+
+    console.log({message1, message2, message3})
+}
+
+result();
+
+console.log("Stop");
+
+/* 
+Output:
+    Start
+    Stop
+    {message1: 'Subscribe to Roadside Coder', message2: 'Like the Javascript Interview Questions video', message3: 'Share the Javascript Interview Questions video'}
+*/
