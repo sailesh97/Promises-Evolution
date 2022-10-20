dfc92b103ec8e0725e8d519309d6de32817f20cc
sailesh97 <saileshpatra997@gmail.com>
2022-10-20 10:42:19 +0530

    race polyfil output

--- Polyfils/Promise_race_polyfil.js
+++ Polyfils/Promise_race_polyfil.js
@@ -1,4 +1,28 @@
-export function promiseRace(promisesArray) {
+function importantAction(username){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Subscribe to ${username}`);
+        },1000);
+    })
+}
+
+function likeTheVideo(video){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Like the ${video} video`)
+        }, 100);
+    })
+}
+
+function shareTheVideo(video){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(`Share the ${video} video`)
+        }, 500);
+    })
+}
+
+function promiseRace(promisesArray) {
     return new Promise((resolve, reject) => {
       promisesArray.forEach((promise) => {
         promise
@@ -6,5 +30,26 @@ export function promiseRace(promisesArray) {
           .catch(reject); // reject outer promise, as and when any of the input promise rejects
       });
     });
-  }
+}
   
+promiseRace([
+    importantAction("Roadside Coder"),
+    likeTheVideo('Javascript Interview Questions'),
+    shareTheVideo('Javascript Interview Questions')
+])
+    .then(res => console.log(res))
+    .catch(err => console.error("Failed:",err)) 
+
+Promise.race([
+    importantAction("Roadside Coder"),
+    likeTheVideo('Javascript Interview Questions'),
+    shareTheVideo('Javascript Interview Questions')
+])
+    .then(res => console.log(res))
+    .catch(err => console.error("Failed:",err)) 
+
+/**
+ * Output:
+ *  Like the Javascript Interview Questions video
+    Like the Javascript Interview Questions video
+ */
