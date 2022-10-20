c9e4f83801ca7f0dc82bc03cb062a2664163774e
sailesh97 <saileshpatra997@gmail.com>
2022-10-20 10:44:31 +0530

    .any polyfil

--- Polyfils/Promise.anyPolyfil.js
+++ Polyfils/Promise.anyPolyfil.js
@@ -1,3 +1,27 @@
+function importantAction(username){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Subscribe to ${username}`);
+        },1000);
+    })
+}
+
+function likeTheVideo(video){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Like the ${video} video`)
+        }, 100);
+    })
+}
+
+function shareTheVideo(video){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(`Share the ${video} video`)
+        }, 500);
+    })
+}
+
 function any(promises) {
     let results = [];
     var counter = 0;
@@ -15,5 +39,28 @@ function any(promises) {
         });
       });
     });
-  };
+};
+
+
+any([
+    importantAction("Roadside Coder"),
+    likeTheVideo('Javascript Interview Questions'),
+    shareTheVideo('Javascript Interview Questions')
+])
+    .then(res => console.log(res))
+    .catch(err => console.error("Failed:",err))
+
+Promise.any([
+    importantAction("Roadside Coder"),
+    likeTheVideo('Javascript Interview Questions'),
+    shareTheVideo('Javascript Interview Questions')
+])
+    .then(res => console.log(res))
+    .catch(err => console.error("Failed:",err))    
+
   
+/**
+ * Output:
+ * Like the Javascript Interview Questions video
+   Like the Javascript Interview Questions video
+ */
